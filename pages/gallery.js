f02562e3bcf2b3d19939009adced2d04d339a246
mc6ril <cyril.lesot@yahoo.fr>
2021-10-09 13:59:41 +0200

    ajout de la gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,11 +1,78 @@
 import Layout from "../components/layout/Layout";
+import json from "../components/slider/dataSlider";
+import Image from "next/image";
+import { useState } from "react";
+import useWindowSize from "../components/functions/Window";
 
 export default function Gallery() {
+  const [activeImage, setActiveImage] = useState(-1);
+
+  const toggleShowImage = (index) => {
+    if (index >= 0) {
+      setActiveImage(index);
+    } else {
+      setActiveImage(-1);
+    }
+  };
+
+  const size = useWindowSize();
+
+  if (!size.height || !size.width) {
+    return "";
+  }
+
   return (
     <Layout page={"Gallerie photo - Gite le Jardin"}>
       <section className="gallery">
-        <h1>Gallerie photo de moyenne taille</h1>
-        <p>Au clic, mettre la photo en grand écran</p>
+        <div className="wrapper">
+          <h1>Le Jardin</h1>
+          <div className="gallery-images">
+            {json.map((image, i) => {
+              return (
+                <div
+                  key={image.id}
+                  className={activeImage === i ? "image active" : "image"}
+                >
+                  {activeImage >= 0 ? (
+                    <Image
+                      src={`/static/images/img${i + 1}.jpg`}
+                      alt={image.title}
+                      layout="responsive"
+                      width={size.width}
+                      height={size.height}
+                      onClick={() => {
+                        toggleShowImage(-1);
+                      }}
+                    />
+                  ) : (
+                    <Image
+                      src={`/static/images/img${i + 1}.jpg`}
+                      alt={image.title}
+                      layout="responsive"
+                      width={250}
+                      height={200}
+                      onClick={() => {
+                        toggleShowImage(i);
+                      }}
+                    />
+                  )}
+
+                  {activeImage >= 0 && (
+                    <div
+                      className="close-image"
+                      onClick={() => {
+                        toggleShowImage(-1);
+                      }}
+                    >
+                      <span></span>
+                      <span></span>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </section>
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,107 +6,44 @@ export default function Accueil() {
     <Layout page={"Accueil - Gite le Jardin"}>
       <section className="home">
         <Slider />
-        <h1>Description du gite</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam impedit laborum
-          atque natus. Voluptatem nesciunt repudiandae repellendus labore, doloribus
-          tempore illum obcaecati. Deleniti corporis saepe obcaecati veritatis eveniet eos
-          nostrum totam optio sit quia hic vitae quasi quae distinctio, qui, odio
-          similique atque facilis laborum accusantium. Quas unde sapiente nihil assumenda
-          dolorum laborum illum ullam alias tempora voluptatum magnam optio, rerum
-          exercitationem cupiditate nostrum ex praesentium, maxime id illo mollitia
-          obcaecati enim? Doloremque rerum velit non laboriosam ratione quasi quae eos
-          pariatur modi blanditiis, recusandae repudiandae fuga illo vero totam deleniti
-          ex voluptate laudantium expedita debitis! Possimus vitae reiciendis odio
-          necessitatibus magnam quibusdam repellendus voluptates illo voluptate cum,
-          voluptatibus voluptas temporibus perferendis aliquam pariatur accusamus? Rem
-          repellendus doloremque enim numquam quis odit, blanditiis ipsa esse, earum
-          voluptates quidem neque inventore vitae alias. Cupiditate ipsum facere
-          accusamus! Unde quia non assumenda, necessitatibus a enim. Modi harum libero
-          natus iste corrupti. Inventore consequatur ipsam laborum veritatis placeat
-          quaerat veniam laboriosam. Deserunt inventore minima quo sint dolore blanditiis
-          amet laborum cum mollitia ex nemo, atque libero, laboriosam recusandae, vel in.
-          Sit tempore iusto consectetur minima tenetur corrupti expedita nam sunt
-          praesentium incidunt laboriosam adipisci, esse itaque est ut debitis qui? Nemo
-          mollitia ex officia eum eaque ab minus numquam necessitatibus similique,
-          laudantium expedita commodi dolor atque deleniti fugiat possimus qui quia
-          architecto? Dicta perferendis optio cumque culpa ipsum libero? Assumenda, culpa
-          sequi aperiam facere sed praesentium rerum neque doloremque porro, accusantium
-          omnis reiciendis aspernatur, odit fugiat a. Voluptatum sit quibusdam, quasi
-          adipisci eius eum et architecto commodi deleniti quidem corporis illum ut autem
-          modi qui! Recusandae dignissimos quam libero nulla laudantium laboriosam iste?
-          Sit sed laborum corrupti eveniet aliquid, harum natus atque doloribus dolore
-          eligendi consequatur et saepe, laudantium ducimus velit eos sequi! Nulla
-          distinctio libero fugiat tempora dignissimos ut accusamus vero ipsa perferendis.
-          Cupiditate cumque facere in ut eos inventore asperiores voluptatem autem illum
-          doloribus, unde obcaecati vitae voluptas hic totam et a alias deserunt impedit
-          consequuntur quo fugit explicabo quod. Consectetur, corrupti. Corporis, sit.
-          Ipsa esse harum ab reprehenderit corrupti maxime explicabo, id animi officia
-          eaque perspiciatis at minima facilis laborum sequi inventore laudantium suscipit
-          quod odit cum, aperiam corporis. Saepe nostrum hic illum ducimus assumenda
-          cumque voluptates omnis vel officiis temporibus dolor tenetur, blanditiis amet
-          velit doloribus sunt similique modi magnam quidem aspernatur? Quasi nam,
-          pariatur maxime odio earum illum repellendus nobis sunt ut distinctio est
-          cupiditate qui tempora, aliquid delectus atque magnam. Ipsa soluta, beatae cum
-          veritatis voluptate similique architecto eius a eligendi nostrum quam temporibus
-          animi distinctio nihil nobis, saepe nesciunt dolorem? Assumenda deserunt
-          accusantium dolorem officiis, excepturi alias libero ducimus. Ex dolores vel,
-          doloremque iure neque et ipsum nemo deserunt impedit sed error quam quibusdam
-          fugit, unde cum amet officia voluptas. Tenetur, aliquid molestiae nulla
-          assumenda laborum ratione numquam dolores commodi dignissimos dicta nisi
-          molestias officiis, ad modi quis expedita magnam iure quos. Provident architecto
-          incidunt tenetur quae debitis veritatis labore tempore molestiae soluta optio
-          qui ut, fugit quo exercitationem nostrum dolores nesciunt nulla officia sapiente
-          illo ipsum! Eos officia quas at non cum laborum nisi, est earum labore
-          consectetur adipisci tenetur harum libero laboriosam, ab cupiditate beatae.
-          Consequatur magni ea sunt repellat ipsam eos numquam amet eum adipisci dolorum.
-          Aut, facere nostrum? Iste dicta quod alias, accusamus minima ut blanditiis
-          quaerat error distinctio neque autem vitae voluptas commodi! Ratione molestias
-          ab amet nulla reiciendis sapiente doloremque eum suscipit hic harum nobis nam
-          sed veritatis illum delectus, quasi sunt doloribus? Eius numquam ad, cupiditate
-          hic quidem fugit veniam aliquam, sit provident accusamus et nobis est blanditiis
-          deserunt nesciunt totam neque eveniet! Magni vero, nemo sit inventore labore,
-          nobis facere, eum unde laborum aliquid obcaecati quis dolor? Sapiente
-          laudantium, esse dolorem reiciendis doloribus nam ad adipisci aut corporis
-          dolore quisquam sunt quo repudiandae est fugiat atque voluptas doloremque qui,
-          possimus recusandae sit ea vel repellat. Consectetur vel ut, illo omnis natus
-          praesentium quasi corporis velit minima similique hic tenetur nulla autem
-          voluptates numquam voluptatum iure! Delectus exercitationem, corporis reiciendis
-          dolor tempore iure magnam enim doloribus quaerat totam quis dignissimos! Tempora
-          ducimus exercitationem delectus. Labore excepturi saepe cupiditate tempora. Non
-          ea architecto doloribus veritatis, voluptates quis beatae reiciendis ab animi
-          totam voluptatum mollitia ullam, aperiam numquam necessitatibus, qui vel. Sunt
-          maxime rem in quibusdam, ipsum corporis nemo laborum fugit adipisci excepturi
-          nihil culpa impedit dolorem deleniti unde nam, fugiat asperiores eos. Dolore,
-          consequatur placeat! Porro expedita qui saepe consequuntur, reiciendis ducimus,
-          dolorum doloribus laborum veritatis ad sunt fuga hic quidem aspernatur tempore
-          dolorem, nisi nostrum doloremque praesentium atque voluptates perspiciatis magni
-          distinctio? Culpa, dicta, nisi quisquam aliquid totam alias, veniam rem quas
-          quasi deserunt eum. Neque cumque animi repudiandae natus in, provident magnam
-          vero corrupti amet. Quas sit obcaecati quia nihil eum, quibusdam voluptatum non
-          labore dicta illo. Laborum animi vitae, sunt minima magnam praesentium dolorum
-          eveniet perspiciatis fugiat consectetur molestiae, expedita non iste quidem
-          voluptates incidunt nam repellendus numquam officiis! Quod iure nostrum adipisci
-          facilis ipsa cupiditate deserunt repellendus amet quos nisi accusantium itaque
-          porro hic, eveniet id, consequuntur ratione ullam quibusdam, modi facere
-          perferendis provident! Ipsa quo suscipit modi alias quis rerum odit pariatur,
-          inventore ad quibusdam nulla. Voluptates possimus voluptate eum, nesciunt,
-          consequatur sint quidem libero quia officia quasi maiores repellendus ipsa,
-          itaque culpa sapiente temporibus ad voluptatem vel suscipit dolorum? Hic, itaque
-          deleniti minus magni eveniet ex saepe ea ullam nemo accusamus consequatur enim
-          similique quae autem soluta. Tempore optio debitis, ipsa aliquam suscipit
-          voluptates quibusdam consequatur tenetur fugiat voluptatem nesciunt perferendis
-          asperiores earum deserunt beatae quod possimus eveniet, laboriosam delectus? Eum
-          fugiat earum totam dignissimos vitae enim exercitationem hic officiis, aut neque
-          ducimus in molestias sed perspiciatis accusamus perferendis cum quod sequi.
-          Inventore at officia ex laborum beatae incidunt tenetur corporis doloribus
-          veniam. Libero quas nesciunt commodi doloribus eum magni cumque ab, harum
-          suscipit deserunt necessitatibus dolorem esse, provident corporis, similique
-          voluptatem? Sit recusandae exercitationem, necessitatibus rem quisquam similique
-          eligendi quo cupiditate repellat pariatur temporibus tenetur in excepturi
-          aspernatur laudantium sunt tempore. Dolor itaque ab accusamus facilis. Voluptate
-          animi sint eum recusandae? Incidunt, harum.
-        </p>
+        <div className="wrapper">
+          <h1>Description du gite (titre à supprimer ensuite)</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam impedit
+            laborum atque natus. Voluptatem nesciunt repudiandae repellendus labore,
+            doloribus tempore illum obcaecati. Deleniti corporis saepe obcaecati veritatis
+            eveniet eos nostrum totam optio sit quia hic vitae quasi quae distinctio, qui,
+            odio similique atque facilis laborum accusantium. Quas unde sapiente nihil
+            assumenda dolorum laborum illum ullam alias tempora voluptatum magnam optio,
+            rerum exercitationem cupiditate nostrum ex praesentium, maxime id illo
+            mollitia obcaecati enim? Doloremque rerum velit non laboriosam ratione quasi
+            quae eos pariatur modi blanditiis, recusandae repudiandae fuga illo vero totam
+            deleniti ex voluptate laudantium expedita debitis! Possimus vitae reiciendis
+            odio necessitatibus magnam quibusdam repellendus voluptates illo voluptate
+            cum, voluptatibus voluptas temporibus perferendis aliquam pariatur accusamus?
+            Rem repellendus doloremque enim numquam quis odit, blanditiis ipsa esse, earum
+            voluptates quidem neque inventore vitae alias. Cupiditate ipsum facere
+            accusamus! Unde quia non assumenda, necessitatibus a enim. Modi harum libero
+            natus iste corrupti. Inventore consequatur ipsam laborum veritatis placeat
+            quaerat veniam laboriosam. Deserunt inventore minima quo sint dolore
+            blanditiis amet laborum cum mollitia ex nemo, atque libero, laboriosam
+            recusandae, vel in. Sit tempore iusto consectetur minima tenetur corrupti
+            expedita nam sunt praesentium incidunt laboriosam adipisci, esse itaque est ut
+            debitis qui? Nemo mollitia ex officia eum eaque ab minus numquam
+            necessitatibus similique, laudantium expedita commodi dolor atque deleniti
+            fugiat possimus qui quia architecto? Dicta perferendis optio cumque culpa
+            ipsum libero? Assumenda, culpa sequi aperiam facere sed praesentium rerum
+            neque doloremque porro, accusantium omnis reiciendis aspernatur, odit fugiat
+            a. Voluptatum sit quibusdam, quasi adipisci eius eum et architecto commodi
+            deleniti quidem corporis illum ut autem modi qui! Recusandae dignissimos quam
+            libero nulla laudantium laboriosam iste? Sit sed laborum corrupti eveniet
+            aliquid, harum natus atque doloribus dolore eligendi consequatur et saepe,
+            laudantium ducimus velit eos sequi! Nulla distinctio libero fugiat tempora
+            dignissimos ut accusamus vero ipsa perferendis. Cupiditate cumque facere in ut
+            eos inventore asperiores voluptatem autem illum doloribus, unde obcaecati
+            vitae voluptas hic totam et a alias deserunt impedit
+          </p>
+        </div>
       </section>
     </Layout>
   );
